4a6f389cee0fc0594c9b02ab188a8e4610be3442
Simon Rogers <s@the-entente.org>
2019-01-25 15:24:32 +0000

    Add Web Viewer

---
+++ Web Viewer/README.md
@@ -0,0 +1,31 @@
+# colophon-variable-fonts-tester
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Deploys to built application to gh-pages repo at: https://github.com/Colophon-Foundry/google-sans
+```
+npm run deploy
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ Web Viewer/src/components/Applications.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="main-container">
+    <md-tabs class="fixed-tabs" @md-changed="tabChanged">
+      <md-tab id="news" md-label="News"></md-tab>
+      <md-tab id="inbox" md-label="Inbox"></md-tab>
+      <md-tab id="email" md-label="Email"></md-tab>
+      <md-tab id="calendar" md-label="Calendar"></md-tab>
+      <md-tab id="search" md-label="Search"></md-tab>
+    </md-tabs>
+    <div :style="typefaceStyle" class="google-text">
+      <Calendar v-if="currentTab === 'calendar'" :variables="variables" />
+      <Inbox v-if="currentTab === 'inbox'" :variables="variables" />
+      <Email v-if="currentTab === 'email'" :variables="variables" />
+      <News v-if="currentTab === 'news'" :variables="variables" />
+      <Search v-if="currentTab === 'search'" :variables="variables" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Calendar from './applications/Calendar.vue';
+import Inbox from './applications/Inbox.vue';
+import Email from './applications/Email.vue';
+import News from './applications/News.vue';
+import Search from './applications/Search.vue';
+export default {
+  name: 'Applications',
+  components: {
+    Calendar,
+    Inbox,
+    Email,
+    News,
+    Search
+  },
+  props: ['variables', 'typefaceStyle'],
+  data() {
+    return {
+      currentTab: 'news'
+    }
+  },
+  methods: {
+    tabChanged(id) {
+      this.currentTab = id;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .fixed-tabs {
+    position: fixed;
+  }
+</style>
