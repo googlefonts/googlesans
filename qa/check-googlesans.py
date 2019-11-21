ba73064ebd187271d60545185769a1a3f04a2cc4
Chris Simpkins <git.simpkins@gmail.com>
2019-11-21 11:00:25 -0500

    Add excluded check ID filter to fontbakery check profile
    Thanks Felipe!

--- qa/check-googlesans.py
+++ qa/check-googlesans.py
@@ -11,21 +11,53 @@ profile_imports = ("fontbakery.profiles.universal",)
 profile = profile_factory(default_section=Section("Google Sans Custom Checks"))
 
 GOOGLESANS_PROFILE_CHECKS = UNIVERSAL_PROFILE_CHECKS + [
-    "com.google.fonts/check/googlesans/testfail",
     "com.google.fonts/check/googlesans/testpass",
 ]
 
-# Test failure template
-@check(
-    id="com.google.fonts/check/googlesans/testfail",
-    rationale="""
-        This is a test failure rationale.
-    """,
+# define check ID's in the upstream `universal` profile
+# that should be excluded here
+excluded_check_ids = (
+    "com.google.fonts/check/ftxvalidator_is_available",
+    # "com.google.fonts/check/os2_metrics_match_hhea",
+    # "com.google.fonts/check/unwanted_tables",
+    # "com.google.fonts/check/dsig",
 )
-def com_google_fonts_check_googlesans_test_fail():
-    """A test failure example."""
-    yield FAIL, "test failure message"
 
+# ================================================
+#
+# Example test implementation
+#
+# ================================================
+
+
+# # Test failure template
+# @check(
+#     id="com.google.fonts/check/googlesans/testfail",
+#     rationale="""
+#         This is a test failure rationale.
+#     """,
+# )
+# def com_google_fonts_check_googlesans_test_fail():
+#     """A test failure example."""
+#     yield FAIL, "test failure message"
+
+
+# # Test pass template
+# @check(
+#     id="com.google.fonts/check/googlesans/testpass",
+#     rationale="""
+#         This is a test pass rationale.
+#     """,
+# )
+# def com_google_fonts_check_googlesans_test_pass():
+#     """A test pass example."""
+#     yield PASS, "test pass message"
+
+# ================================================
+#
+# Begin check definitions
+#
+# ================================================
 
 # Test pass template
 @check(
@@ -39,5 +71,20 @@ def com_google_fonts_check_googlesans_test_pass():
     yield PASS, "test pass message"
 
 
+# ================================================
+#
+# End check definitions
+#
+# ================================================
+
+# skip filter function to exclude checks defined in the
+# fontbakery universal profile
+def check_skip_filter(checkid, font=None, **iterargs):
+    if font and checkid in excluded_check_ids:
+        return False, ("Check skipped in Google Sans profile")
+    return True, None
+
+
+profile.check_skip_filter = check_skip_filter
 profile.auto_register(globals())
 profile.test_expected_checks(GOOGLESANS_PROFILE_CHECKS, exclusive=True)
