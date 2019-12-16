3726bb0eade04811f03d7073d945957a8f8c9cb3
Chris Simpkins <git.simpkins@gmail.com>
2019-12-16 13:26:33 -0500

    [check-googlesans.py] add fontbakery check for OS/2.fsSelection bit 7

--- qa/check-googlesans.py
+++ qa/check-googlesans.py
@@ -11,7 +11,7 @@ profile_imports = ("fontbakery.profiles.universal",)
 profile = profile_factory(default_section=Section("Google Sans Custom Checks"))
 
 GOOGLESANS_PROFILE_CHECKS = UNIVERSAL_PROFILE_CHECKS + [
-    "com.google.fonts/check/googlesans/testpass",
+    "com.google.fonts/check/googlesans/opentype/os2/fsselectionbit7",
 ]
 
 # define check ID's in the upstream `universal` profile
@@ -21,8 +21,12 @@ excluded_check_ids = (
     "com.google.fonts/check/dsig",
     # "com.google.fonts/check/os2_metrics_match_hhea",
     # "com.google.fonts/check/unwanted_tables",
-
 )
+
+ATTRIBUTES = {
+    "os2_fsselection_bit7": 1,
+}
+
 
 # ================================================
 #
@@ -60,16 +64,37 @@ excluded_check_ids = (
 #
 # ================================================
 
-# Test pass template
+# ================================================
+# OpenType table attribute checks
+# ================================================
+
+# OS/2.fsSelection bit 7 (USE_TYPO_METRICS) is set in all fonts
 @check(
-    id="com.google.fonts/check/googlesans/testpass",
+    id="com.google.fonts/check/googlesans/opentype/os2/fsselectionbit7",
     rationale="""
-        This is a test pass rationale.
+        Confirms that fonts have OS/2.fsSelection bit 7 (USE_TYPO_METRICS) set \
+        for typo vertical metrics (instead of win vertical metrics)
     """,
 )
-def com_google_fonts_check_googlesans_test_pass():
-    """A test pass example."""
-    yield PASS, "test pass message"
+def com_google_fonts_check_googlesans_opentype_os2_fsselectionbit7(ttFonts):
+    """OS/2.fsSelection bit 7 (USE_TYPO_METRICS) is set in all fonts"""
+    os2_fsselection_bit7_isset = ATTRIBUTES["os2_fsselection_bit7"] == 1
+
+    found_fail = False
+    fail_list = []
+    for tt in ttFonts:
+        fsselection_int = tt["OS/2"].fsSelection
+        fsselection_bit_is_set_test = (fsselection_int & (1 << 7)) != 0
+        if fsselection_bit_is_set_test is os2_fsselection_bit7_isset:
+            pass
+        else:
+            found_fail = True
+            fail_list.append(tt.reader.file.name)
+
+    if found_fail:
+        yield FAIL, f"The OS/2.fsSelection bit 7 (USE_TYPO_METRICS) was NOT set in the following fonts: {fail_list}."
+    else:
+        yield PASS, "The OS/2.fsSelection bit 7 (USE_TYPO_METRICS) was set in all fonts."
 
 
 # ================================================
