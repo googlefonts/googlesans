07e08ef532f6c8d9c61dd5962403b4b0f7134075
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2021-02-26 11:46:46 +0000

    yield FAIL when VF instance entry can't be found

--- qa/check-fea.py
+++ qa/check-fea.py
@@ -400,12 +400,20 @@ def com_google_fonts_check_googlesans_features_regression(ttFont, hb_font):
                 assert isinstance(shaped_texts_expected, dict)
 
                 for key, shaped_text in shaped_texts.items():
-                    if shaped_text != shaped_texts_expected[key]:
+                    try:
+                        shaped_texts_expected_instance = shaped_texts_expected[key]
+                    except KeyError as e:
+                        yield FAIL, (
+                            f"{shaping_file}: No entry found for instance {str(e)} of "
+                            f"{filename.name}"
+                        )
+                        continue
+                    if shaped_text != shaped_texts_expected_instance:
                         shaped_texts_str = textwrap.indent(
                             "\n".join(shaped_text), "\t  "
                         )
                         shaped_texts_expected_str = textwrap.indent(
-                            "\n".join(shaped_texts_expected[key]), "\t  "
+                            "\n".join(shaped_texts_expected_instance), "\t  "
                         )
                         yield FAIL, (
                             f"{shaping_file}: Expected and actual shaping not matching."
