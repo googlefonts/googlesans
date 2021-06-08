7b601aca14f877ec73408aab24d60f17d953a6aa
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2021-06-08 12:17:33 +0100

    Try harder to match source and target sources

--- scripts/gs-merge-designspace.py
+++ scripts/gs-merge-designspace.py
@@ -115,6 +115,21 @@ if skip_export_glyphs_target:
     designspace_target.lib[SKIP_EXPORT_GLYPHS_KEY] = skip_export_glyphs
 
 
+def canonical_location(
+    location: Dict[str, float], designspace: DesignSpaceDocument
+) -> Dict[str, float]:
+    """Returns a canonical location from a raw Designspace location.
+
+    Vendor sources are inconsistent, so using user values and tags to match
+    source axes is hopefully more reliable.
+    """
+    name_to_axis = {a.name: a for a in designspace.axes}
+    name_to_tag = {a.name: a.tag for a in designspace.axes}
+    return {
+        name_to_tag[k]: name_to_axis[k].map_backward(v) for k, v in location.items()
+    }
+
+
 # Actually import now.
 for import_source in designspace_import.sources:
     if import_source.layerName is not None:
@@ -122,9 +137,10 @@ for import_source in designspace_import.sources:
         sys.exit(1)
 
     # Fill in the defaults if the import DS does not have e.g. a GRAD axis.
+    # Match axes by tags because those are more consistent across vendor sources.
     import_source_location = {
-        **designspace_target.default.location,
-        **import_source.location,
+        **canonical_location(designspace_target.default.location, designspace_target),
+        **canonical_location(import_source.location, designspace_import),
     }
 
     # Match import to target UFO.
@@ -132,7 +148,10 @@ for import_source in designspace_import.sources:
         target_source = next(
             s
             for s in designspace_target.sources
-            if s.location == import_source_location
+            if (
+                canonical_location(s.location, designspace_target)
+                == import_source_location
+            )
         )
     except StopIteration:
         try:
