bd6e3723ea4970f5aa8867bec654bdb551d334eb
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2022-05-10 17:17:04 +0100

    WIP: Add post-processing for the cut instances

--- scripts/internal/cut-instances.py
+++ scripts/internal/cut-instances.py
@@ -20,12 +20,25 @@ import multiprocessing.pool
 import subprocess
 import sys
 from pathlib import Path
+from types import SimpleNamespace
+from black import Any
 
 from fontTools.designspaceLib import DesignSpaceDocument
+from fontTools.ttLib import TTFont
+from fontTools.ttLib.tables.O_S_2f_2 import Panose
+from ufo2ft.fontInfoData import getAttrWithFallback, normalizeStringForPostscript
+import ufoLib2
 
 
 def cut_instance(
-    variable_font: Path, user_location: dict[str, float], output_file: Path
+    variable_font: Path,
+    user_location: dict[str, float],
+    panose_values: list[int],
+    family_name: str | None,
+    style_name: str | None,
+    stylemap_family_name: str | None,
+    stylemap_style_name: str | None,
+    output_file: Path,
 ) -> None:
     user_location_args = [f"{k}={v}" for k, v in user_location.items()]
 
@@ -43,6 +56,74 @@ def cut_instance(
             *user_location_args,
         ]
     )
+
+    font = TTFont(output_file)
+
+    panose = Panose()
+    panose.bFamilyType = panose_values[0]
+    panose.bSerifStyle = panose_values[1]
+    panose.bWeight = panose_values[2]
+    panose.bProportion = panose_values[3]
+    panose.bContrast = panose_values[4]
+    panose.bStrokeVariation = panose_values[5]
+    panose.bArmStyle = panose_values[6]
+    panose.bLetterForm = panose_values[7]
+    panose.bMidline = panose_values[8]
+    panose.bXHeight = panose_values[9]
+    font["OS/2"].panose = panose
+
+    info = {
+        "familyName": family_name,
+        "styleName": style_name,
+        "styleMapFamilyName": stylemap_family_name,
+        "styleMapStyleName": stylemap_style_name,
+    }
+    build_name_entries(info, font["name"])
+
+    font.save(output_file)
+
+
+def build_name_entries(info: dict[str, Any], name: Any) -> None:
+    info = ufoLib2.objects.Info(**info)
+
+    familyName = getAttrWithFallback(info, "styleMapFamilyName")
+    styleName = getAttrWithFallback(info, "styleMapStyleName").title()
+    preferredFamilyName = getAttrWithFallback(info, "openTypeNamePreferredFamilyName")
+    preferredSubfamilyName = getAttrWithFallback(
+        info, "openTypeNamePreferredSubfamilyName"
+    )
+    fullName = f"{preferredFamilyName} {preferredSubfamilyName}"
+
+    nameVals = {
+        1: familyName,
+        2: styleName,
+        4: fullName,
+        6: getAttrWithFallback(info, "postscriptFontName"),
+        16: preferredFamilyName,
+        17: preferredSubfamilyName,
+    }
+    print(nameVals)
+
+    # don't add typographic names if they are the same as the legacy ones
+    if nameVals[1] == nameVals[16]:
+        del nameVals[16]
+    if nameVals[2] == nameVals[17]:
+        del nameVals[17]
+    # postscript font name
+    if nameVals[6]:
+        nameVals[6] = normalizeStringForPostscript(nameVals[6])
+
+    for nameId in sorted(nameVals.keys()):
+        nameVal = nameVals[nameId]
+        if not nameVal:
+            continue
+        platformId = 3
+        platEncId = 1
+        langId = 0x409
+        # Set built name record if not set yet
+        if name.getName(nameId, platformId, platEncId, langId):
+            continue
+        name.setName(nameVal, nameId, platformId, platEncId, langId)
 
 
 def main(args: list[str] | None = None) -> int:
@@ -66,14 +147,32 @@ def main(args: list[str] | None = None) -> int:
     name2tag = {axis.name: axis.tag for axis in designspace.axes}
     name2axis = {axis.name: axis for axis in designspace.axes}
     for instance in designspace.instances:
+        custom_parameters = dict(instance.lib["com.schriftgestaltung.customParameters"])
         user_location = {
             name2tag[k]: name2axis[k].map_backward(v)
             for k, v in instance.location.items()
         }
         output_file = output_dir / Path(instance.filename).with_suffix(".ttf").name
 
+        family_name = instance.familyName
+        style_name = instance.styleName
+        stylemap_family_name = instance.styleMapFamilyName
+        stylemap_style_name = instance.styleMapStyleName
+
         processes.append(
-            pool.apply_async(cut_instance, (variable_font, user_location, output_file))
+            pool.apply_async(
+                cut_instance,
+                (
+                    variable_font,
+                    user_location,
+                    custom_parameters["panose"],
+                    family_name,
+                    style_name,
+                    stylemap_family_name,
+                    stylemap_style_name,
+                    output_file,
+                ),
+            )
         )
 
     pool.close()
