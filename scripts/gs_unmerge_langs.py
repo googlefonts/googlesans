0b1b915db8027934cec7497f4a8622b41acc3f2f
Harry Dalton <harry.dalton@daltonmaag.com>
2024-09-24 17:22:01 +0100

    Make subset definitions explicit
    This gives us somewhere to put the new typo metrics - just placeholder
    values for now - and also decouples the subset names from the subset
    _list_ names, which will allow us to combine multiple scripts into a
    single subset.

--- scripts/gs_unmerge_langs.py
+++ scripts/gs_unmerge_langs.py
@@ -22,17 +22,55 @@ appended to the font's file name, with output fonts landing in
 build/GoogleSans/android. Each subset includes Latin.
 """
 
-from argparse import ArgumentParser
 import multiprocessing
+from argparse import ArgumentParser
+from dataclasses import dataclass
 from pathlib import Path
 
-from fontTools.subset import main as pyftsubset, Subsetter
-
+from fontTools.subset import Subsetter
+from fontTools.subset import main as pyftsubset
 
 BUILD_DIR = Path("build/GoogleSans/android")
-
 SUBSETS_DIR = Path("source/GoogleSans/subsets")
-SUBSET_FILES = sorted(SUBSETS_DIR.glob("*.txt"))
+
+
+@dataclass(frozen=True, kw_only=True)
+class Subset:
+    """All of the ingredients to make a subset."""
+
+    name: str
+    metrics: tuple[int, int]  # TODO: Apply
+
+    @property
+    def codepoints(self) -> list[Path]:
+        return [SUBSETS_DIR / f"{self.name}.txt"]
+
+
+SUBSETS = [
+    Subset(name="Armn", metrics=(-1000, 2000)),
+    Subset(name="Beng", metrics=(-1000, 2000)),
+    Subset(name="Cyrl", metrics=(-1000, 2000)),
+    Subset(name="Deva", metrics=(-1000, 2000)),
+    Subset(name="Ethi", metrics=(-1000, 2000)),
+    Subset(name="Geor", metrics=(-1000, 2000)),
+    Subset(name="Grek", metrics=(-1000, 2000)),
+    Subset(name="Gujr", metrics=(-1000, 2000)),
+    Subset(name="Guru", metrics=(-1000, 2000)),
+    Subset(name="Hebr", metrics=(-1000, 2000)),
+    Subset(name="Khmr", metrics=(-1000, 2000)),
+    Subset(name="Knda", metrics=(-1000, 2000)),
+    Subset(name="Laoo", metrics=(-1000, 2000)),
+    Subset(name="Latn", metrics=(-1000, 2000)),
+    Subset(name="Mlym", metrics=(-1000, 2000)),
+    Subset(name="Orya", metrics=(-1000, 2000)),
+    Subset(name="Sinh", metrics=(-1000, 2000)),
+    Subset(name="Taml", metrics=(-1000, 2000)),
+    Subset(name="Telu", metrics=(-1000, 2000)),
+    Subset(name="Thai", metrics=(-1000, 2000)),
+    Subset(name="Zinh", metrics=(-1000, 2000)),
+    Subset(name="Zyyy", metrics=(-1000, 2000)),
+    Subset(name="Zzzz", metrics=(-1000, 2000)),
+]
 
 
 def main(ttfs: list[Path]) -> None:
@@ -46,17 +84,17 @@ def extract_subsets(base_ttf: Path) -> None:
     with multiprocessing.Pool() as pool:
         pool.starmap(
             extract_subset,
-            ((base_ttf, unicodes_path) for unicodes_path in SUBSET_FILES),
+            ((base_ttf, subset) for subset in SUBSETS),
         )
 
 
-def extract_subset(base_ttf: Path, unicodes_list_path: Path) -> None:
-    output_path = BUILD_DIR / f"{base_ttf.stem}-{unicodes_list_path.stem}.ttf"
+def extract_subset(base_ttf: Path, subset: Subset) -> None:
+    output_path = BUILD_DIR / f"{base_ttf.stem}-{subset.name}.ttf"
     print(f"Making {output_path} with pyftsubset")
 
     args = (
         str(base_ttf),
-        f"--unicodes-file={unicodes_list_path}",
+        *(f"--unicodes-file={path}" for path in subset.codepoints),
         f"--output-file={output_path}",
     )
     try:
@@ -64,13 +102,13 @@ def extract_subset(base_ttf: Path, unicodes_list_path: Path) -> None:
     except Subsetter.MissingGlyphsSubsettingError as e:
         missing_glyphs = sorted(e.args[0])
         print(
-            f"pyftsubset of {unicodes_list_path.stem}",
+            f"pyftsubset of {subset.name}",
             "failed due to missing glyphs:\n-",
             "\n- ".join(missing_glyphs),
         )
         raise
     except:
-        print(f"pyftsubset of {unicodes_list_path.stem} failed")
+        print(f"pyftsubset of {subset.name} failed")
         raise
 
 
