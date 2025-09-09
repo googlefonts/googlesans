9aa1a16317c03bb22697e199e68588fdbdd111ba
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2025-09-09 15:42:47 +0100

    Fix lint

--- scripts/one-off/ymetrics/ymetrics-shaped.py
+++ scripts/one-off/ymetrics/ymetrics-shaped.py
@@ -321,7 +321,8 @@ def load_test_words(
     else:
         raise ValueError(
             "Download the AOSP aosp.json to scripts/diffenator2-data to have "
-            "that be used as well: https://github.com/googlefonts/aosp-test-texts/blob/main/corpus/aosp.json"
+            "that be used as well: "
+            "https://github.com/googlefonts/aosp-test-texts/blob/main/corpus/aosp.json"
         )
 
     return [Word(word, source) for word, source in sorted(words_by_source.items())]
