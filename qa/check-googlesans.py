4816d804f213ce3caac0e706bca6cbf0c219fcf8
Ricky Atkins <ricky.atkins@daltonmaag.com>
2026-05-28 13:08:42 +0100

    Port checks to Fontspector profile & plugins

--- qa/check-googlesans.py
+++ qa/check-googlesans.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env -S uv run --script
+
 # Copyright 2020 Google Sans Authors
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,122 +14,253 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# /// script
+# dependencies = [
+#     "fontspectorapi",
+#     "fontTools",
+# ]
+# ///
+
+
 from pathlib import Path
-import os
+from typing import cast
 
-# Hack to have this be conditional but without appending later
-# Only run on main & releases:
-# https://github.com/googlefonts/googlesans/issues/316#issuecomment-2120825286
-# https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
-INTERPOLATION_ISSUES = (
-    ()
-    if "CI" in os.environ
-    and (
-        os.environ.get("GITHUB_REF_NAME") == "main"
-        or os.environ.get("GITHUB_REF_TYPE") == "tag"
-    )
-    else ("interpolation_issues",)
+from fontspectorapi import (
+    FAIL,
+    PASS,
+    SKIP,
+    WARN,
+    CheckStatuses,
+    Message,
+    Plugin,
+    check,
+    plugin_main,
 )
+from fontspectorapi.utils import (
+    UNICODERANGE_DATA,
+    chars_in_range,
+    compute_unicoderange_bits,
+    unicoderange,
+    unicoderange_bit_name,
+)
+from fontTools.ttLib import TTFont
 
-PROFILE = {
-    "include_profiles": ["universal"],
-    # The checks are in a separate file, because Simon says that eventually this
-    # file will become pure data.
-    "check_definitions": [Path(__file__).parent / "check-googlesans-checks.py"],
-    "sections": {
-        "Google Fonts Checks": ["varfont/duplexed_axis_reflow"],
-        "Google Sans Custom Checks": [
-            "googlesans/opentype/os2/fsselectionbit7",
-            "googlesans/opentype/os2/winascent",
-            "googlesans/opentype/os2/windescent",
-            "googlesans/opentype/hhea/ascent",
-            "googlesans/opentype/hhea/descent",
-            "googlesans/opentype/hhea/linegap",
-            "googlesans/opentype/os2/strikeout",
-            "googlesans/opentype/os2/typodescender",
-            "googlesans/opentype/os2/typoascender",
-            "googlesans/opentype/os2/typolinegap",
-            "googlesans/opentype/os2/unicode_range_bits",
-            "googlesans/opentype/post/underline",
-            "googlesans/opentype/BASE",
-            "googlesans/vf/fvaraxes",
-            "googlesans/vf/fvardefault",
-            "varfont/duplexed_axis_reflow",
-        ],
-    },
-    "exclude_checks": [
-        # replaced by custom checks
-        "family/win_ascent_and_descent",
-        # we do want our opsz definition
-        "opentype/fvar/regular_coords_correct",
-        # "os2_metrics_match_hhea",
-        # "unwanted_tables",
-        # too many unactionable warnings
-        "outline_jaggy_segments",
-        # design rather than QA problem
-        "outline_semi_vertical",
-        # design rather than QA problem
-        "contour_count",
-        "opentype/varfont/valid_default_instance_nameids",  # Bogus
-        "varfont/bold_wght_coord",  # Buggy in 0.8.9
-        # https://github.com/googlefonts/googlesans/issues/576:
-        "soft_dotted",
-        # We intentionally set the family names as they are
-        "opentype/family/consistent_family_name",
-        # See declaration
-        *INTERPOLATION_ISSUES,
-        # Ignore issue about a Khmer glyph that Cadson-Demak confirmed to be OK
-        # https://github.com/googlefonts/googlesans/pull/656#issuecomment-3130940361
-        "base_has_width",
-        # Ignore mac names, Marianna confirmed we don't change the fonts at the moment
-        # https://github.com/googlefonts/googlesans/pull/656#issuecomment-3103220308
-        "no_mac_entries",
-        # Ignore family name issue, it's on purpose
-        # https://github.com/googlefonts/googlesans/pull/656#issuecomment-3103220308
-        "typographic_family_name",
-        # Not applicable to Google Sans fonts:
-        "fontdata_namecheck",
-        # We're not that worried about Microsoft Office support
-        # https://github.com/googlefonts/googlesans/issues/693#issuecomment-3480871914
-        "name/family_and_style_max_length",
-        # https://github.com/googlefonts/googlesans/issues/712
-        "smallcaps_before_ligatures",
-        # Arabic glyphs fail these but shape fine when feature code is ran
-        "empty_letters",
-    ],
-    "configuration_defaults": {
-        "googlesans/opentype/os2/fsselectionbit7": {
-            "os2_fsselection_bit7": 1
-        },
-        "googlesans/opentype/hhea/ascent": {
-            "hhea_ascent": 966  # set to match typo metrics values
-        },
-        "googlesans/opentype/hhea/descent": {
-            "hhea_descent": -286  # set to match typo metrics values
-        },
-        "googlesans/opentype/hhea/linegap": {"hhea_linegap": 0},
-        "googlesans/opentype/os2/strikeout": {
-            "os2_strikeout_position": 306,
-            "os2_strikeout_size": 84,
-        },
-        "googlesans/opentype/os2/typodescender": {
-            "os2_typodescender": -286,
-        },
-        "googlesans/opentype/os2/typoascender": {
-            "os2_typoascender": 966,  # set to match hhea metrics values
-        },
-        "googlesans/opentype/os2/typolinegap": {
-            "os2_typolinegap": 0,
-        },
-        "googlesans/vf/fvaraxes": {
-            "expected_fvar_axes": ["opsz", "wght", "GRAD"]
-        },
-        "googlesans/vf/fvardefault": {
-            "axis_defaults": {"opsz": 18.0, "wght": 400.0, "grad": 0.0}
-        },
-        "googlesans/opentype/post/underline": {
-            "post_underline_position": -160,
-            "post_underline_thickness": 84,
-        },
-    },
+FVAR_DEFAULTS = {
+    "opsz": 18.0,
+    "wght": 400.0,
+    "GRAD": 0.0,
 }
+
+
+# ================================================
+#
+# Begin check definitions
+#
+# ================================================
+
+
+# ================================================
+# OpenType table attribute checks
+# ================================================
+
+
+@check(
+    id="googlesans/os2/fsselectionbit7",
+    title="Use typo metrics",
+    rationale="""
+    Confirms that fonts have OS/2.fsSelection bit 7 (USE_TYPO_METRICS) set \
+    for typo vertical metrics (instead of win vertical metrics)
+    """,
+    runs_on_collection=True,
+)
+def opentype_os2_fsselectionbit7(font_paths: list[Path]) -> CheckStatuses:
+    found_fail = False
+    fail_list = []
+    for font_path in font_paths:
+        ttf = TTFont(font_path)
+        fsselection_int = cast(int, ttf["OS/2"].fsSelection)  # type: ignore
+        fsselection_bit_is_set_test = (fsselection_int & (1 << 7)) != 0
+        if not fsselection_bit_is_set_test:
+            found_fail = True
+            fail_list.append(font_path)
+
+    if found_fail:
+        yield (
+            FAIL,
+            f"The OS/2.fsSelection bit 7 (USE_TYPO_METRICS) was NOT set "
+            f"in the following fonts: {' '.join(fail_list)}.",
+        )
+    else:
+        yield (
+            PASS,
+            "The OS/2.fsSelection bit 7 (USE_TYPO_METRICS) was set in all fonts.",
+        )
+
+
+@check(
+    id="googlesans/opentype/BASE",
+    title="Font has BASE table",
+    rationale="Checks that the font has a BASE table",
+)
+def has_base_table(font_path: Path) -> CheckStatuses:
+    ttf = TTFont(font_path)
+
+    if "BASE" in ttf:
+        yield PASS, "BASE table present in font"
+    else:
+        yield FAIL, "Missing BASE table"
+
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::
+# Other metrics
+# ::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+@check(
+    id="googlesans/opentype/os2/unicode_range_bits",
+    title="Ensure UnicodeRange bits are properly set",
+    rationale="""
+        When the UnicodeRange bits on the OS/2 table are not properly set, some programs
+        running on Windows may not recognize the font and use a system fallback font
+        instead. For that reason, this check calculates the proper settings by inspecting
+        the glyphs declared on the cmap table and then ensures that their corresponding
+        ranges are enabled.
+    """,
+)
+def unicode_range_bits(font_path: Path) -> CheckStatuses:
+    ttf = TTFont(font_path)
+    unicode_range = unicoderange(ttf)
+    expected_unicoderange = compute_unicoderange_bits(ttf)
+    difference = unicode_range ^ expected_unicoderange
+    if not difference:
+        yield PASS, "Unicode range bits are properly set"
+    else:
+        for bit in range(128):
+            if difference & (1 << bit):
+                range_name = unicoderange_bit_name(bit)
+                num_chars = len(chars_in_range(ttf, bit))
+                range_size = sum(
+                    entry[3] - entry[2] + 1 for entry in UNICODERANGE_DATA[bit]
+                )
+                if num_chars == 0:
+                    set_unset = "0"
+                    num_chars = "none"
+                    yield (
+                        FAIL,
+                        Message(
+                            "bad-range-bit",
+                            f"UnicodeRange bit {bit} '{range_name}' should be {set_unset} "
+                            f"because cmap has {num_chars} of the {range_size} codepoints "
+                            f"in this range.",
+                        ),
+                    )
+                else:
+                    set_unset = "1"
+                    yield (
+                        WARN,
+                        Message(
+                            "bad-range-bit",
+                            f"UnicodeRange bit {bit} '{range_name}' should be {set_unset} "
+                            f"because cmap has {num_chars} of the {range_size} codepoints "
+                            f"in this range.",
+                        ),
+                    )
+
+
+# ================================================
+# Variable build format specific
+# ================================================
+
+
+@check(
+    id="googlesans/vf/fvaraxes",
+    title="Ensure all expected axes are present",
+    rationale="""
+    Confirms that the variable font format builds include
+    all expected axis tags
+    """,
+)
+def variable_fvar_axes(font_path: Path) -> CheckStatuses:
+    ttf = TTFont(font_path)
+
+    if "fvar" not in ttf:
+        yield SKIP, "Not a VF"
+        return
+
+    observed_axis_list = []
+    for axis in ttf["fvar"].axes:  # type: ignore
+        observed_axis_list.append(axis.axisTag)
+
+    if len(observed_axis_list) != len(FVAR_DEFAULTS):
+        yield (
+            FAIL,
+            f"{font_path.name} does not include the correct axis tags. \n"
+            f"Observed: {observed_axis_list}\n"
+            f"Expected: {FVAR_DEFAULTS.keys()}",
+        )
+
+    has_all_tags = True
+    for axis_tag in FVAR_DEFAULTS.keys():
+        if axis_tag not in observed_axis_list:
+            has_all_tags = False
+            yield FAIL, f"{font_path.name} does not include axis tag {axis_tag}"
+
+    if has_all_tags:
+        yield PASS, f"{font_path.name} includes all expected axis tags"
+
+
+@check(
+    id="googlesans/vf/fvardefault",
+    title="Ensure correct axis defaults",
+    rationale="""
+    Confirms that the variable font format builds include the expected fvar
+    default definitions for the Google Sans design axes
+    """,
+)
+def variable_fvar_default(font_path: Path) -> CheckStatuses:
+    ttf = TTFont(font_path)
+
+    if "fvar" not in ttf:
+        yield SKIP, "Not a VF"
+        return
+
+    for axis in ttf["fvar"].axes:  # type: ignore
+        tag = axis.axisTag
+        if tag not in FVAR_DEFAULTS:
+            continue
+        if axis.defaultValue != FVAR_DEFAULTS[tag]:
+            yield (
+                FAIL,
+                f"{font_path.name} does not include the correct "
+                f"fvar {tag} axis default.\n"
+                f"Found: `{axis.defaultValue}` and expected `{FVAR_DEFAULTS[tag]}`",
+            )
+        else:
+            yield (
+                PASS,
+                f"{font_path.name} contains the expected fvar {tag} default.",
+            )
+
+
+# ================================================
+#
+# End check definitions
+#
+# ================================================
+
+
+def register(plugin: Plugin) -> None:
+    CHECKS = (
+        opentype_os2_fsselectionbit7,
+        has_base_table,
+        unicode_range_bits,
+        variable_fvar_axes,
+        variable_fvar_default,
+    )
+    plugin.register_simple_profile(
+        "gs-custom", CHECKS, section_name="Google Sans Custom Checks"
+    )
+
+
+if __name__ == "__main__":
+    raise SystemExit(plugin_main(register, plugin_name="gs-custom"))
